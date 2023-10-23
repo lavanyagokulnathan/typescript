e5d79d53a51d5825501cc0624e064851f07d6824
lavanyagokulnathan <lavanyagokulnathan@outlook.com>
2023-10-23 09:14:34 -0400

    Update and rename exercise to exercise.ts

---
+++ exercise.ts
@@ -0,0 +1,17 @@
+# primitive types
+let age : number = 18;
+
+# Interface - used to create custom types
+interface Contact {
+  id: number,
+  name: string,
+  birthDate: Date
+}
+
+interface Address {
+  line1: string,
+  line2: string,
+  province: string,
+  postalCode: string
+}
+
